bfab929b83e44c9d3adf48329e3fc68cfe3bef6a
Minh Nguyen Van <minh.nguyen@katalon.com>
2022-05-14 11:46:37 +0700

    first commit

---
+++ Test Suites/TS_VisualTest_ClockWorld.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TS_VisualTest_ClockWorld</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>1</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>f71d9480-23e2-40c2-b6fd-bf7829f1296a</testSuiteGuid>
+   <testCaseLink>
+      <guid>cecfbdc2-92eb-4559-837e-eb141e0db5bd</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC004_ClockWorld</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
