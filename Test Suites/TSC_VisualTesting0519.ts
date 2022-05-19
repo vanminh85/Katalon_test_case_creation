bf69378bef9c193768e064b12b9c1e4078802c01
Minh Nguyen <minh.nguyen@katalon.com>
2022-05-19 14:41:28 +0700

    commit

--- Test Suites/TSC_VisualTesting0519.ts
+++
@@ -1,9 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<TestSuiteCollectionEntity>
-   <description></description>
-   <name>TSC_VisualTesting0519</name>
-   <tag></tag>
-   <executionMode>SEQUENTIAL</executionMode>
-   <maxConcurrentInstances>4</maxConcurrentInstances>
-   <testSuiteRunConfigurations/>
-</TestSuiteCollectionEntity>
---
+++ Test Suites/TSC_VisualTesting0520.ts
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteCollectionEntity>
+   <description></description>
+   <name>TSC_VisualTesting0520</name>
+   <tag></tag>
+   <executionMode>SEQUENTIAL</executionMode>
+   <maxConcurrentInstances>4</maxConcurrentInstances>
+   <testSuiteRunConfigurations>
+      <TestSuiteRunConfiguration>
+         <configuration>
+            <groupName>Web Desktop</groupName>
+            <profileName>default</profileName>
+            <runConfigurationId>Chrome</runConfigurationId>
+         </configuration>
+         <runEnabled>true</runEnabled>
+         <testSuiteEntity>Test Suites/TS_VisualTest1</testSuiteEntity>
+      </TestSuiteRunConfiguration>
+   </testSuiteRunConfigurations>
+</TestSuiteCollectionEntity>
