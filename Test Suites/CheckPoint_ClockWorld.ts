2fe37d9206f127f2a019a6316a66737d88bde768
minh.nguyen <minh.nguyen@katalon.com>
2022-07-06 16:22:42 +0700

    add exacmple

--- Test Suites/CheckPoint_ClockWorld.ts
+++ Test Suites/CheckPoint_ClockWorld.ts
@@ -12,33 +12,9 @@
    <rerunImmediately>true</rerunImmediately>
    <testSuiteGuid>f71d9480-23e2-40c2-b6fd-bf7829f1296a</testSuiteGuid>
    <testCaseLink>
-      <guid>1b264668-cba4-4832-91e9-d768337ccba3</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/Checkpoint_Clock</testCaseId>
-   </testCaseLink>
-   <testCaseLink>
       <guid>d9a5fcd6-8bb6-4977-8374-e91f8d23a392</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/CheckPoint_Google</testCaseId>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>cecfbdc2-92eb-4559-837e-eb141e0db5bd</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/CheckPoint_ClockWorld</testCaseId>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>c1898e72-984a-46a2-a855-c4fd0b34c385</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/CheckPoint_ClockWorld_b</testCaseId>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>02a74b79-da4a-4886-a98f-7eb1e7954c66</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/CheckPoint_ClockWorld_c</testCaseId>
    </testCaseLink>
 </TestSuiteEntity>
